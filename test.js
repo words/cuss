f5c6a99a81a9522ef1571026f529395847da5d72
Titus Wormer <tituswormer@gmail.com>
2022-11-07 10:49:10 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {cuss} from './index.js'
 
-test('cuss', (t) => {
-  t.equal(typeof cuss, 'object', 'should be an object #1')
+test('cuss', () => {
+  assert.equal(typeof cuss, 'object', 'should be an object #1')
 
-  t.ok('asshat' in cuss, 'should contain words #1')
-  t.ok('addict' in cuss, 'should contain words #2')
-  t.ok('beaver' in cuss, 'should contain words #3')
+  assert.ok('asshat' in cuss, 'should contain words #1')
+  assert.ok('addict' in cuss, 'should contain words #2')
+  assert.ok('beaver' in cuss, 'should contain words #3')
 
-  t.equal(cuss.asshat, 2, 'should map to a rating #1')
-  t.equal(cuss.addict, 1, 'should map to a rating #2')
-  t.equal(cuss.beaver, 0, 'should map to a rating #3')
-
-  t.end()
+  assert.equal(cuss.asshat, 2, 'should map to a rating #1')
+  assert.equal(cuss.addict, 1, 'should map to a rating #2')
+  assert.equal(cuss.beaver, 0, 'should map to a rating #3')
 })
